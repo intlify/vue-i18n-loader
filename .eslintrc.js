4d1d525c7066a8da1ace6da5bfe0de6b80fe2b4b
kazuya kawaguchi <kawakazu80@gmail.com>
2019-07-15 02:01:50 +0900

    :fire: chore: upgrade dev env

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue-libs/recommended'
+  ],
+  plugins: [],
+  parserOptions: {
+    ecmaVersion: 2015
+  },
+  rules: {
+  }
+}
