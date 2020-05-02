29f04c57f2febab0377f6c60732922deee404ad1
kazuya kawaguchi <kawakazu80@gmail.com>
2020-05-02 17:58:43 +0900

    update shipjs configration

--- ship.config.js
+++ ship.config.js
@@ -36,7 +36,7 @@ async function commitChangelog(current, next) {
 }
 
 module.exports = {
-  mergeStrategy: { toSameBranch: ['master'] },
+  mergeStrategy: { toSameBranch: ['next'] },
   monorepo: undefined,
   updateChangelog: false,
   beforeCommitChanges: ({ nextVersion, exec, dir }) => {
